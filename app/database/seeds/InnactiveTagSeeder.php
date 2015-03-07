33b5dace4f594d4a81ecb69ce9031e78963b8cb2
Lukas Vyslocky <lukas.vyslocky@gmail.com>
2015-03-07 21:38:03 +0000

    updated gitignore + seeder

--- app/database/seeds/InnactiveTagSeeder.php
+++ app/database/seeds/InnactiveTagSeeder.php
@@ -4,7 +4,7 @@ extends DatabaseSeeder
 {
     public function run()
     {
-        $tags = ["unknown","in","and","are","for","the","to","its","can","even","you","dont","is","sometimes","than","at","a","with","other","this","million","from","last","of","your","after","knows","wants","daily","new","more","on","has","an","updated","how","could","without","into","that","turn","will","soon","one","just","lot","now","not","yourself","thanks","what","know","about","but","others","look","them","out","''","off","looking","long","only","newest","youre","isnt",""];
+        $tags = ["unknown","in","and","are","for","the","to","its","can","even","you","dont","is","sometimes","than","at","a","with","other","this","million","from","last","of","your","after","knows","wants","daily","new","more","on","has","an","updated","how","could","without","into","that","turn","will","soon","one","just","lot","now","not","yourself","thanks","what","know","about","but","others","look","them","out","''","off","looking","long","only","newest","youre","isnt","","arrears","-","continue","ease","show","roundup","biggest"];
         foreach ($tags as $tag)
         {
             $tag = new Tag();
